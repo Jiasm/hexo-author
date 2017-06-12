cc9d8eb982f5ae7fc75de08da2507446ca4ba173
jiasm <jiashunming@blued.com>
2017-06-12 16:41:35 +0800

    add avatar support

--- index.js
+++ index.js
@@ -8,20 +8,35 @@
 const child_process = require('child_process')
 const fs = require('fs')
 
-hexo.on('new', (post) => {
-  let lines = post.content.split('\n')
-  let username = getText(child_process.execSync('git config user.name'))
-  let email = getText(child_process.execSync('git config user.email'))
+if (typeof hexo !== 'undefined') {
+  hexo.on('new', (post) => {
+    let lines = post.content.split('\n')
+    let username = getText(child_process.execSync('git config user.name'))
+    let email = getText(child_process.execSync('git config user.email'))
 
-  lines.splice(1, 0, 'email: ' + email)
-  lines.splice(1, 0, 'author: ' + username)
+    // 如果有username 并且可以通过API获取到信息 则把头像也塞进去
+    if (username) {
+      let gitInfo = getText(child_process.execSync(`curl https://api.github.com/users/${username}`))
+      if (gitInfo) {
+        gitInfo = JSON.parse(gitInfo)
 
-  post.content = lines.join('\n')
-  if (post.path !== false) {
-    fs.writeFile(post.path, post.content)
+        let avatar = gitInfo.avatar_url
+        lines.splice(1, 0, 'avatar: ' + avatar)
+      }
+    }
+
+    lines.splice(1, 0, 'email: ' + email)
+    lines.splice(1, 0, 'author: ' + username)
+
+    post.content = lines.join('\n')
+    if (post.path !== false) {
+      fs.writeFile(post.path, post.content)
+    }
+  })
+
+  function getText (buffer) {
+    return buffer ? buffer.toString().replace(/\r?\n?/g, '') : 'unknown'
   }
-})
-
-function getText (buffer) {
-  return buffer ? buffer.toString().replace(/\r?\n?/g, '') : 'unknown'
+} else {
+  console.log('hexo is not defined')
 }
